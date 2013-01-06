0b6c986dbfeaa6eeae580f2abcf2850ff680c584
chunjiangshieh <chunjiang.shieh@gmail.com>
2013-01-06 18:09:40 +0800

    commit

--- gen/com/xcj/android/net/R.java
+++ gen/com/xcj/android/net/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.xcj.android.touchgallery.example;
+package com.xcj.android.net;
 
 public final class R {
     public static final class attr {
