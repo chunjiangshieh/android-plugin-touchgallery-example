eeda822a82b83daa4dcb1978cfffac361a73d7ea
chunjiangshieh <chunjiang.shieh@gmail.com>
2013-01-06 14:20:24 +0800

    second commit

---
+++ gen/com/xcj/android/touchgallery/example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.xcj.android.touchgallery.example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
